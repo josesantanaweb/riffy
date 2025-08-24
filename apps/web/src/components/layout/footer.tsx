1daaaecd5c72080eaa54bd28148b97081c1e15c0
josnielfermin <josniel@esthetiqo.com>
2025-08-24 18:07:07 -0400

    feat: add new icons and styles for the application
    - Added IconSearch, IconUser, and IconWhatsapp components with SVGs.
    - Updated index.ts to export new icons.
    - Added SVG files for various icons including arrow-back, calendar, chevron-down, chevron-left, chevron-right, copy, filter, gift, instagram, search, user, and whatsapp.
    - Introduced a new palette.css file for theming with dark and light modes.
    - Created a tsconfig.json file for TypeScript configuration.

---
+++ apps/web/src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export const Footer = () => {
+  return (
+    <footer className="w-full text-center px-4 h-20 text-gray-2 font-medium text-[10px] flex items-center justify-center">
+      © 2025 Riffy Para Gestion de Rifas Online. Todos los derechos reservados
+    </footer>
+  );
+};
