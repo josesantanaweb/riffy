7b2036aa03be48f8f0cbfaa7a40757d556231d98
josesantanaoky <josesantana@okyapp.com>
2025-08-29 23:00:59 -0400

    fix: utils import

--- packages/riffy-components/src/components/icon/Icon.tsx
+++ packages/riffy-components/src/components/icon/Icon.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import type { ReactElement } from 'react';
-import { cn } from '@/utils/cn';
+import { cn } from '../../utils/cn';
 
 export type IconName =
   | 'credit-card'
