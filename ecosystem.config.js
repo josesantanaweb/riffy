c1c8a4d04d6a3ee49612750fe61559423c378c17
josesantanaoky <josesantana@okyapp.com>
2025-09-19 17:59:30 -0400

    feat: add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api',
+      script: 'apps/api/dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'web',
+      script: 'apps/web/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'admin',
+      script: 'apps/admin/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
