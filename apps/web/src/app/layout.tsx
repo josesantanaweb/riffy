6b09680dc66df56cd63f59879f0815b0ae44fe31
josesantanaoky <josesantana@okyapp.com>
2025-10-14 18:17:08 -0400

    fix: light theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" className="dark">
       <body>
         <Providers>
           <Layout>{children}</Layout>
